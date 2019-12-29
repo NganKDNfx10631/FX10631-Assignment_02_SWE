95e5cf440330e58f566e53d866a64141a4748810
Funix FX00424 <anhndfx00424@funix.edu.vn>
2019-12-29 23:43:58 +0700

    Add convert str time to millisecond

--- script/subtitle/subtitleHandling.js
+++ script/subtitle/subtitleHandling.js
@@ -2,8 +2,10 @@ class SubtitleHandling {
    static parseSub(data){
       let result = [];
       let items = data.split("\n\r\n");
+      let self = this;
       $.each(items, function(index, el) {
          let cut = el.split("\r\n");
+         let time = cut[1].split("-->");
          try {
             result.push(cut[2].trim());
          } catch (e) {
@@ -12,6 +14,7 @@ class SubtitleHandling {
       });
       return result;
    }
+
    static parseYoutubeSub(f){
       let result = [];
       let pattern = /(\d+)\n([\d:,]+)\s+-{2}\>\s+([\d:,]+)\n([\s\S]*?(?=\n{2}|$))\n([\s\S]*?(?=\n{2}|$))/gm;
@@ -24,5 +27,18 @@ class SubtitleHandling {
          result.push(matches[4] +"\r\n"+ matches[5]);
       }
       return result;
+   }
+
+   static timeToMillisecond(str) {
+      str = str.replace(".", ",");
+      let splits = str.split(":");
+      let millisecond = 0;
+      millisecond += parseInt(splits[0])*3600*1000;
+      millisecond += parseInt(splits[1])*60*1000;
+      let cut = splits[2].split(",");
+      millisecond += parseInt(cut[0])*1000;
+      millisecond += parseInt(cut[1]);
+
+      return millisecond;
    }
 }
