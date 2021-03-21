555450a4ac61a646297766b7c61aea5479510b9a
huy <phamhuy1989@gmail.com>
2021-03-21 12:00:45 +0700

    Fix bug coursera.

--- script/subtitle/courseraSubtitle.js
+++ script/subtitle/courseraSubtitle.js
@@ -22,8 +22,8 @@ function initComponent(){
    });
 }
 function createElement(mode){
-      //let container = $('<div class="vjs-react c-video vjs-fluid video-js vjs-controls-disabled vjs-workinghover vjs-has-started vjs-paused vjs-user-inactive" id="funix-text" dir="ltr" tabindex="0" aria-live="assertive" lang="en" draggable="true" data-layer="4" style="touch-action: none;text-align: left;overflow: hidden;left: 0;width: 100%;height: 50px;bottom: 2%;"> <span class="captions-text" style="overflow-wrap: normal; display: block;"> <span class="caption-visual-line" style="display: block;"> <span class="ytp-caption-segment" style="word-wrap: break-word;width: 100%;display: inline-block;white-space: pre-wrap;background: rgba(8, 8, 8, 0.5);-webkit-box-decoration-break: clone;border-radius: 5.20556px;font-size: 2.5vw;color: rgb(255, 255, 255);fill: rgb(255, 255, 255);font-family: &quot;YouTube Noto&quot;, Roboto, &quot;Arial Unicode Ms&quot;, Arial, Helvetica, Verdana, &quot;PT Sans Caption&quot;, sans-serif;"> </span> </span> </span> </div>');
-      let container = $("<div class='closed-captions is-visible' style='display: block;padding-top:2.5px; background-color: grey; color: white; height : 30px;text-align: center' lang='funix' id = 'funix-text'><div>");
+      //let container = $('<div class="vjs-react c-video vjs-fluid video-js vjs-controls-disabled vjs-workinghover vjs-has-started vjs-paused vjs-user-inactive" id="funix-text" dir="ltr" tabindex="0" aria-live="assertive" lang="en" draggable="true" data-layer="4" style="touch-action: none;text-align: left;overflow: hidden;left: 5%;width: 90%;height: auto;bottom: 2%;position: absolute;"> <span class="captions-text" style="overflow-wrap: normal; display: block;bottom: 2%;position: absolute;"> <span class="caption-visual-line" style="display: block;"> <span class="ytp-caption-segment" style="word-wrap: break-word;width: 100%;display: inline-block;white-space: pre-wrap;background: rgba(8, 8, 8, 0.5);-webkit-box-decoration-break: clone;border-radius: 5.20556px;font-size: 2.5vw;color: rgb(255, 255, 255);fill: rgb(255, 255, 255);font-family: &quot;YouTube Noto&quot;, Roboto, &quot;Arial Unicode Ms&quot;, Arial, Helvetica, Verdana, &quot;PT Sans Caption&quot;, sans-serif;"> </span> </span> </span> </div>');
+      let container = $('<div class="vjs-react c-video vjs-fluid video-js vjs-controls-disabled vjs-workinghover vjs-has-started vjs-paused vjs-user-inactive" id="funix-text" dir="ltr" tabindex="0" aria-live="assertive" lang="en" draggable="true" data-layer="4" style="touch-action: none;text-align: left;overflow: hidden;left: 5%;width: 90%;height: auto;bottom: 2%;position: absolute;font-size: 18px"> <span class="captions-text" style="overflow-wrap: normal; display: block;position: absolute;"> <span class="caption-visual-line" style="display: block;"> <span class="ytp-caption-segment" style="word-wrap: break-word;width: 100%;display: inline-block;white-space: pre-wrap;background: rgba(8, 8, 8, 0.5);-webkit-box-decoration-break: clone;border-radius: 5.20556px;font-size: 2.5vw;color: rgb(255, 255, 255);fill: rgb(255, 255, 255);font-family: &quot;YouTube Noto&quot;, Roboto, &quot;Arial Unicode Ms&quot;, Arial, Helvetica, Verdana, &quot;PT Sans Caption&quot;, sans-serif;"> </span> </span> </span> </div>');
       //setTimeout(() => {
       //courseraSubtitleObserver = new subtitleObserver("#funix-text .ytp-caption-segment");
       courseraSubtitleObserver = new subtitleObserver("#funix-text");
@@ -33,7 +33,7 @@ function createElement(mode){
          
       startObserver();
       
-      $(".rc-VideoMiniPlayer").append(container);
+      $(".rc-VideoControlsContainer").append(container);
       //}, 1000);
 
       let funixSubtitle = document.getElementById("funix-text");
