ef25e9951172f1153cdf5f307312c17c63ca6afd
aanhpt@gmail.com <@Anhpt109972>
2022-03-01 11:01:18 +0700

    anhpt 58 - fix the error of not showing the coursera source popup

--- script/subtitle/courseraSubtitle.js
+++ script/subtitle/courseraSubtitle.js
@@ -1,195 +1,221 @@
 let courseraSubtitleObserver, vi, eng, jp, oldURL, currentURL;
-function initComponent() {
-   initData().then((status) => {
-      if (status) {
-         getSettingData().then(res => {
-            let subtitleMode = res.modeSubtitle;
-            if (subtitleMode === "0") {
-               let video = $("video"); // check tag video
-               if (!video || video.length == 0)
-                  return false;
+var isShowPopup = false;
 
-               Notifycation.confirmSubtitle().then(mode => {
-                  if (mode !== 0) {
-                     createElement(mode);
-                  }
-               });
-            }
-            // else if (subtitleMode === "0") {
-            //    createElement(1);
-            // } else if (subtitleMode === "2") {}
-         })
-      }
-   });
+function initComponent() {
+    if (checkPopup())
+        return false;
+
+    initData().then((status) => {
+        if (status) {
+            getSettingData().then(res => {
+                let subtitleMode = res.modeSubtitle;
+                if (subtitleMode === "0") {
+                    let video = $("video"); // check tag video
+                    if (!video || video.length == 0)
+                        return false;
+
+                    isShowPopup = true;
+                    Notifycation.confirmSubtitle().then(mode => {
+                        if (mode !== 0) {
+                            createElement(mode);
+                        }
+                    });
+                }
+                // else if (subtitleMode === "0") {
+                //    createElement(1);
+                // } else if (subtitleMode === "2") {}
+            })
+        }
+    });
 }
 
 function checkURLchange(currentURL) {
-   if (currentURL != oldURL) {
-      oldURL = currentURL;
-      initComponent();
-   }
+    if (currentURL != oldURL) {
+        oldURL = currentURL;
+        initComponent();
+    } else {
+        oldURL = window.location.pathname;
+    }
 
-   oldURL = window.location.pathname;
-   setTimeout(function () {
-      checkURLchange(window.location.pathname);
-   }, 1000);
+    setTimeout(function () {
+        checkURLchange(window.location.pathname);
+    }, 1000);
 }
 
 function createElement(mode) {
-   //let container = $('<div class="vjs-react c-video vjs-fluid video-js vjs-controls-disabled vjs-workinghover vjs-has-started vjs-paused vjs-user-inactive" id="funix-text" dir="ltr" tabindex="0" aria-live="assertive" lang="en" draggable="true" data-layer="4" style="touch-action: none;text-align: left;overflow: hidden;left: 5%;width: 90%;height: auto;bottom: 2%;position: absolute;"> <span class="captions-text" style="overflow-wrap: normal; display: block;bottom: 2%;position: absolute;"> <span class="caption-visual-line" style="display: block;"> <span class="ytp-caption-segment" style="word-wrap: break-word;width: 100%;display: inline-block;white-space: pre-wrap;background: rgba(8, 8, 8, 0.5);-webkit-box-decoration-break: clone;border-radius: 5.20556px;font-size: 2.5vw;color: rgb(255, 255, 255);fill: rgb(255, 255, 255);font-family: &quot;YouTube Noto&quot;, Roboto, &quot;Arial Unicode Ms&quot;, Arial, Helvetica, Verdana, &quot;PT Sans Caption&quot;, sans-serif;"> </span> </span> </span> </div>');
-   //let container = $('<div class="vjs-react c-video vjs-fluid video-js vjs-controls-disabled vjs-workinghover vjs-has-started vjs-paused vjs-user-inactive" id="funix-text" dir="ltr" tabindex="0" aria-live="assertive" lang="en" draggable="true" data-layer="4" style="touch-action: none;text-align: center;overflow: hidden;left: 5%;width: 90%;height: 42px;bottom: 2%;position: absolute;font-size: 18px"> <span class="captions-text" style="overflow-wrap: normal; display: block;position: absolute;"> <span class="caption-visual-line" style="display: block;"> <span class="ytp-caption-segment" style="word-wrap: break-word;width: 100%;display: inline-block;white-space: pre-wrap;background: rgba(8, 8, 8, 0.5);-webkit-box-decoration-break: clone;border-radius: 5.20556px;font-size: 2.5vw;color: rgb(255, 255, 255);fill: rgb(255, 255, 255);font-family: &quot;YouTube Noto&quot;, Roboto, &quot;Arial Unicode Ms&quot;, Arial, Helvetica, Verdana, &quot;PT Sans Caption&quot;, sans-serif;"> </span> </span> </span> </div>');
+    //let container = $('<div class="vjs-react c-video vjs-fluid video-js vjs-controls-disabled vjs-workinghover vjs-has-started vjs-paused vjs-user-inactive" id="funix-text" dir="ltr" tabindex="0" aria-live="assertive" lang="en" draggable="true" data-layer="4" style="touch-action: none;text-align: left;overflow: hidden;left: 5%;width: 90%;height: auto;bottom: 2%;position: absolute;"> <span class="captions-text" style="overflow-wrap: normal; display: block;bottom: 2%;position: absolute;"> <span class="caption-visual-line" style="display: block;"> <span class="ytp-caption-segment" style="word-wrap: break-word;width: 100%;display: inline-block;white-space: pre-wrap;background: rgba(8, 8, 8, 0.5);-webkit-box-decoration-break: clone;border-radius: 5.20556px;font-size: 2.5vw;color: rgb(255, 255, 255);fill: rgb(255, 255, 255);font-family: &quot;YouTube Noto&quot;, Roboto, &quot;Arial Unicode Ms&quot;, Arial, Helvetica, Verdana, &quot;PT Sans Caption&quot;, sans-serif;"> </span> </span> </span> </div>');
+    //let container = $('<div class="vjs-react c-video vjs-fluid video-js vjs-controls-disabled vjs-workinghover vjs-has-started vjs-paused vjs-user-inactive" id="funix-text" dir="ltr" tabindex="0" aria-live="assertive" lang="en" draggable="true" data-layer="4" style="touch-action: none;text-align: center;overflow: hidden;left: 5%;width: 90%;height: 42px;bottom: 2%;position: absolute;font-size: 18px"> <span class="captions-text" style="overflow-wrap: normal; display: block;position: absolute;"> <span class="caption-visual-line" style="display: block;"> <span class="ytp-caption-segment" style="word-wrap: break-word;width: 100%;display: inline-block;white-space: pre-wrap;background: rgba(8, 8, 8, 0.5);-webkit-box-decoration-break: clone;border-radius: 5.20556px;font-size: 2.5vw;color: rgb(255, 255, 255);fill: rgb(255, 255, 255);font-family: &quot;YouTube Noto&quot;, Roboto, &quot;Arial Unicode Ms&quot;, Arial, Helvetica, Verdana, &quot;PT Sans Caption&quot;, sans-serif;"> </span> </span> </span> </div>');
 
-   let container = $('<div style ="position: absolute;bottom: 4em;left: 0;right: 0;z-index: 1;display: flex;justify-content: center;-webkit-transition: bottom .2s;-moz-transition: bottom .2s;-ms-transition: bottom .2s;-o-transition: bottom .2s; transition: bottom .2s;cursor: inherit; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none;-o-user-select: none;user-select: none;"> <div style = "position: relative;display: inline;height: auto;max-width: 30em;color: #fff;background-color: #14171c;font-family: sans-serif;line-height: 1.4;text-align: center;margin: 0 .5em 1em; padding: 2px 8px;white-space: pre-line;writing-mode: horizontal-tb;unicode-bidi: plaintext;direction: ltr;-webkit-box-decoration-break: clone;font-size: 2.5vh; opacity: 0.75;" id="funix-text"></div> </div>');
-   //setTimeout(() => {
-   //courseraSubtitleObserver = new subtitleObserver("#funix-text .ytp-caption-segment");
-   courseraSubtitleObserver = new subtitleObserver("#funix-text");
-   // alert(JSON.stringify(vi));
-   courseraSubtitleObserver.initData(vi, eng, jp);
-   courseraSubtitleObserver.mode = mode;
+    let container = $('<div style ="position: absolute;bottom: 4em;left: 0;right: 0;z-index: 1;display: flex;justify-content: center;-webkit-transition: bottom .2s;-moz-transition: bottom .2s;-ms-transition: bottom .2s;-o-transition: bottom .2s; transition: bottom .2s;cursor: inherit; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none;-o-user-select: none;user-select: none;"> <div style = "position: relative;display: inline;height: auto;max-width: 30em;color: #fff;background-color: #14171c;font-family: sans-serif;line-height: 1.4;text-align: center;margin: 0 .5em 1em; padding: 2px 8px;white-space: pre-line;writing-mode: horizontal-tb;unicode-bidi: plaintext;direction: ltr;-webkit-box-decoration-break: clone;font-size: 2.5vh; opacity: 0.75;" id="funix-text"></div> </div>');
+    //setTimeout(() => {
+    //courseraSubtitleObserver = new subtitleObserver("#funix-text .ytp-caption-segment");
+    courseraSubtitleObserver = new subtitleObserver("#funix-text");
+    // alert(JSON.stringify(vi));
+    courseraSubtitleObserver.initData(vi, eng, jp);
+    courseraSubtitleObserver.mode = mode;
+    //var audio = '<audio id="audio-speech" src="https://file01.fpt.ai/text2speech-v5/short/2022-02-24/3915385e88a7c540be4f58885e50028a.mp3" controls="controls" style="width: 100%;display: none"></audio>';
+    //$(".rc-VideoControlsContainer").append(audio);
 
-   startObserver();
+    startObserver();
+    $(".rc-VideoControlsContainer").append(container);
+    //}, 1000);
 
-   $(".rc-VideoControlsContainer").append(container);
-   //}, 1000);
-
-   let funixSubtitle = document.getElementById("funix-text");
-
-   if (funixSubtitle === undefined || funixSubtitle === null) {
-      setTimeout(function () {
-         createElement(mode);
-      }, 1000);
-   }
+    let funixSubtitle = document.getElementById("funix-text");
+    if (funixSubtitle === undefined || funixSubtitle === null) {
+        setTimeout(function () {
+            createElement(mode);
+        }, 1000);
+    }
 }
+
+function setCurTime(time = 0) {
+    var audio = document.getElementById("audio-speech");
+    audio.currentTime = time;
+}
+
 function startObserver() {
-   let video = $("video").get(0);
-   if (video !== undefined) {
-      courseraSubtitleObserver.startObserver(video);
-   } else {
-      setTimeout(function () {
-         startObserver();
-      }, 1000);
-   }
+    let video = $("video").get(0);
+    if (video !== undefined) {
+        courseraSubtitleObserver.startObserver(video);
+        //setCurTime(video.currentTime);
+    } else {
+        setTimeout(function () {
+            startObserver();
+        }, 1000);
+    }
 }
+
 async function initData() {
-   let id = window.location.pathname.split("/").splice(-1)[0];
-   let request = {
-      content: "POST Request",
-      requestUrl: "https://funix-subtitle.firebaseapp.com/get",
-      requestBody: {
-         cid: "coursera",
-         lid: id
-      }
-   };
-   let resAPI = {};
-   await sendMessagePromise(request).then(res => {
-      resAPI = res;
-   });
+    let id = window.location.pathname.split("/").splice(-1)[0];
+    let request = {
+        content: "POST Request",
+        requestUrl: "https://funix-subtitle.firebaseapp.com/get",
+        requestBody: {
+            cid: "coursera",
+            lid: id
+        }
+    };
+    let resAPI = {};
+    await sendMessagePromise(request).then(res => {
+        resAPI = res;
+    });
 
-   if (resAPI.code === 200) {
-      // check duplicate id
-      if (resAPI.data.numb !== undefined) {
-         let pathFile = window.location.pathname;
-         let numb = resAPI.data.numb;
-         let isNew = "1";
-         for (var i = 1; i <= numb; i++) {
-            let request2 = {
-               content: "POST Request",
-               requestUrl: "https://funix-subtitle.firebaseapp.com/get",
-               requestBody: {
-                  cid: "coursera",
-                  lid: id + "_" + i.toString()
-               }
-            };
-            let resAPI2 = {};
-            await sendMessagePromise(request2).then(res => {
-               resAPI2 = res;
-            });
-            if (resAPI2.code === 200) {
-               if (resAPI2.data.url !== undefined && resAPI2.data.url.includes(pathFile)) {
-                  await sendMessagePromise({
-                     content: "GET Request",
-                     requestUrl: resAPI2.data.vi,
-                  }).then(data => {
-                     if (data !== undefined) {
+    if (resAPI.code === 200) {
+        // check duplicate id
+        if (resAPI.data.numb !== undefined) {
+            let pathFile = window.location.pathname;
+            let numb = resAPI.data.numb;
+            let isNew = "1";
+            for (var i = 1; i <= numb; i++) {
+                let request2 = {
+                    content: "POST Request",
+                    requestUrl: "https://funix-subtitle.firebaseapp.com/get",
+                    requestBody: {
+                        cid: "coursera",
+                        lid: id + "_" + i.toString()
+                    }
+                };
+                let resAPI2 = {};
+                await sendMessagePromise(request2).then(res => {
+                    resAPI2 = res;
+                });
+                if (resAPI2.code === 200) {
+                    if (resAPI2.data.url !== undefined && resAPI2.data.url.includes(pathFile)) {
+                        await sendMessagePromise({
+                            content: "GET Request",
+                            requestUrl: resAPI2.data.vi,
+                        }).then(data => {
+                            if (data !== undefined) {
+                                vi = SubtitleHandling.parseSubByRegex(data);
+                            }
+                        });
+
+                        await sendMessagePromise({
+                            content: "GET Request",
+                            requestUrl: resAPI2.data.jp,
+                        }).then(data => {
+                            if (data !== undefined) {
+                                jp = SubtitleHandling.parseSubByRegex(data);
+                            }
+                        });
+                        isNew = "2";
+                    }
+                }
+            }
+            if (isNew === "2") {
+
+            }
+            else {
+                await sendMessagePromise({
+                    content: "GET Request",
+                    requestUrl: resAPI.data.vi,
+                }).then(data => {
+                    if (data !== undefined) {
                         vi = SubtitleHandling.parseSubByRegex(data);
-                     }
-                  });
+                    }
+                });
 
-                  await sendMessagePromise({
-                     content: "GET Request",
-                     requestUrl: resAPI2.data.jp,
-                  }).then(data => {
-                     if (data !== undefined) {
+                await sendMessagePromise({
+                    content: "GET Request",
+                    requestUrl: resAPI.data.jp,
+                }).then(data => {
+                    if (data !== undefined) {
                         jp = SubtitleHandling.parseSubByRegex(data);
-                     }
-                  });
-                  isNew = "2";
-               }
+                    }
+                });
             }
-         }
-         if (isNew === "2") {
 
-         }
-         else {
-            await sendMessagePromise({
-               content: "GET Request",
-               requestUrl: resAPI.data.vi,
-            }).then(data => {
-               if (data !== undefined) {
-                  vi = SubtitleHandling.parseSubByRegex(data);
-               }
-            });
+        } else {
 
             await sendMessagePromise({
-               content: "GET Request",
-               requestUrl: resAPI.data.jp,
+                content: "GET Request",
+                requestUrl: resAPI.data.vi,
             }).then(data => {
-               if (data !== undefined) {
-                  jp = SubtitleHandling.parseSubByRegex(data);
-               }
+                // alert("data : " + JSON.stringify(data));
+                if (data !== undefined) {
+                    vi = SubtitleHandling.parseSubByRegex(data);
+                }
             });
-         }
+            // await sendMessagePromise({
+            //    content: "GET Request",
+            //    requestUrl: resAPI.data.en,
+            // }).then(data => {
+            //    self.en = SubtitleHandling.parseSub(data);
+            // });
 
-      } else {
+            await sendMessagePromise({
+                content: "GET Request",
+                requestUrl: resAPI.data.jp,
+            }).then(data => {
+                if (data !== undefined) {
+                    jp = SubtitleHandling.parseSubByRegex(data);
+                }
+            });
 
-         await sendMessagePromise({
-            content: "GET Request",
-            requestUrl: resAPI.data.vi,
-         }).then(data => {
-            // alert("data : " + JSON.stringify(data));
-            if (data !== undefined) {
-               vi = SubtitleHandling.parseSubByRegex(data);
-            }
-         });
-         // await sendMessagePromise({
-         //    content: "GET Request",
-         //    requestUrl: resAPI.data.en,
-         // }).then(data => {
-         //    self.en = SubtitleHandling.parseSub(data);
-         // });
-
-         await sendMessagePromise({
-            content: "GET Request",
-            requestUrl: resAPI.data.jp,
-         }).then(data => {
-            if (data !== undefined) {
-               jp = SubtitleHandling.parseSubByRegex(data);
-            }
-         });
-
-      }
-      return true;
-   }
-   return false;
+        }
+        return true;
+    }
+    return false;
 }
 
 
 $(document).ready(function () {
-   //(new CourseraSubtitle()).run();
-   oldURL = window.location.pathname;
-   currentURL = window.location.pathname;
-   checkURLchange(oldURL);
-   setTimeout(function () {
-      initComponent();
-   }, 3000);
+    //(new CourseraSubtitle()).run();
+    oldURL = window.location.pathname;
+    currentURL = window.location.pathname;
+    var timeCountDown = setInterval(function () {
+        initComponent();
+        if (isShowPopup) {
+            clearInterval(timeCountDown);
+            checkURLchange(oldURL);
+        }
+    }, 3000);
 });
+
+function checkPopup() {
+    let popup = $(".jconfirm-modern"); // check tag video
+    if (!popup || popup.length == 0)
+        return false;
+    else
+        return true;
+}
